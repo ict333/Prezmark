662f618d54d1011995a4279cd6bf3cdc9dee3b72
re-tr0 <re-tr0@ubuntu>
2017-02-25 23:46:36 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>this is a test page</title>
+    </head>
+    <body>
+        <?php
+            echo '<h1>Hello World</h1>';// put your code here
+        ?>
+    </body>
+</html>
