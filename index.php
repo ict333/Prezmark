4b8bca52502e5b6259df4411306c1d1cecd78cad
esha <esha@esha-HP-Pavilion-15-Notebook-PC>
2017-03-25 17:17:18 +0400

    Merge origin/master
    Conflicts:
    	AssessPresentations.php
    	FillPresentationSchedule.php

---
+++ index.php
@@ -0,0 +1,25 @@
+
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    
+    <body>
+    <div class="header">
+         <a href="index.php"> <img src="logo.png"></a>
+        <nav>
+            <a href="" class="active">About</a>
+            <a href="MarkerLogin.php">Marker</a>
+            <a href="SuperUserLogin.php">Super User</a>
+        </nav>
+    </div>
+    <div id="separator"></div>
+
+
+    
+    </body>
+    <footer>
+           &#169;2017 All rights reserved by Murdoch University 
+    </footer>  
+</html>
+
