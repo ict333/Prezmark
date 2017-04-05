23a8083feeae1e2636aeb20854d25999744ab484
retr0 <retr0@ubuntu>
2017-04-05 12:45:06 +0400

    - FIxed bug with backup page - Added links to other pages on admin page

--- Backup.php
+++ Backup.php
@@ -1,4 +1,10 @@
-<?php //session_start();
+<?php 
+session_start();
+$role= $_SESSION['Role'];
+if($role!="Admin")
+{
+    header("Location: CreateSuperUser.php");
+}
   ini_set('display_errors',1);
     error_reporting (E_ALL);
     if(isset($_POST['submit']))
@@ -7,10 +13,9 @@
         $backupFile = 'Prezmark_' . date("d-m-Y") . '.sql';
         header('Content-Type: text/sql');
         header('Content-Disposition: attachment; filename="'.$backupFile.'"');
-        $path=$_POST['Path'];
         
-        $command = "mysqldump --user=root --password=123 --host=localhost "
-                . "Prezmark ";
+        $command = "mysqldump --user=team71 --password=IUfkjs*454ds --host=localhost "
+                . "team71 ";
         passthru($command);
         exit(0);
         
@@ -23,9 +28,14 @@
         <link rel="icon" href="icon.png" type="image/x-icon"></link>
     </head>
     
-    <body>
-     <div class="header">
+    <div class="header">
          <a href="index.php"> <img src="logo.png"></a>
+        <nav>
+            <a href="DisableAccounts.php">Disable Account</a>
+            <a href="CreateSuperUser.php" >Create Super User</a>
+            <a href="Backup.php" class="active">Create Backup</a>
+            <a href="Logout.php">Logout</a>
+        </nav>
     </div>
     <div id="separator"></div>
 
